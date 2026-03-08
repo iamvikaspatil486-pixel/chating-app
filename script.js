99cd68f2a530de6c2d0503e40730e51dc3e8c70c
iamvikaspatil486-pixel <iamvikaspatil486@gmail.com>
2026-03-08 11:08:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // 🔹 REPLACE WITH YOUR OWN
-const SUPABASE_URL = "ntfglwfrhljjkzecifuh";
+const SUPABASE_URL = "https://ntfglwfrhljjkzecifuh.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50Zmdsd2ZyaGxqamt6ZWNpZnVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1OTEyNTYsImV4cCI6MjA4ODE2NzI1Nn0.xVC4IFBD72prT7KS-jiHlRQixVrR81QUVX2av_jU7uM";
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 // Select buttons
 const registerBtn = document.querySelector(".register");
 const loginBtn = document.querySelector(".login");
